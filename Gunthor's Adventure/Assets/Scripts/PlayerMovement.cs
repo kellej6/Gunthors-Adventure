7145f37c2115b4652ae3f18314053915b281f6bb
Jason Keller <kellej6@ucmail.uc.edu>
2021-03-04 13:34:58 -0500

    Player Animations
    The player can move in the cardinal directions and the animation blend trees are applied correctly.

--- Gunthor's Adventure/Assets/Scripts/PlayerMovement.cs
+++ Gunthor's Adventure/Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,13 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
+    private Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,9 +22,21 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
+        MoveAndAnimate();
+    }
+
+    private void MoveAndAnimate()
+    {
         if (change != Vector3.zero)
         {
+            myAnimator.SetBool("isWalking", true);
+            myAnimator.SetFloat("moveX", change.x);
+            myAnimator.SetFloat("moveY", change.y);
             MoveCharacter();
+        }
+        else
+        {
+            myAnimator.SetBool("isWalking", false);
         }
     }
 
