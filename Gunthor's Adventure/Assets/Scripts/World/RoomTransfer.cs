637c851b7890a50678fc347ca82fd1b8e462e106
Jason Keller <kellej6@ucmail.uc.edu>
2021-04-23 23:21:06 -0400

    Interior Scene

--- Gunthor's Adventure/Assets/Scripts/World/RoomTransfer.cs
+++ Gunthor's Adventure/Assets/Scripts/World/RoomTransfer.cs
@@ -27,7 +27,7 @@ public class RoomTransfer : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !collision.isTrigger)
         {
             cameraMovement.minPosition += cameraChange;
             cameraMovement.maxPosition += cameraChange;
